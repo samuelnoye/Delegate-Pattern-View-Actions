39ec67a4636476eaec99757539a9d322bd6b90e6
Samuel Noye <samuel.noye@amalitech.org>
2022-05-20 17:27:28 +0000

    Update ViewController.swift

--- Delegate Pattern & View Actions/Delegate Pattern & View Actions/ViewController.swift
+++ Delegate Pattern & View Actions/Delegate Pattern & View Actions/ViewController.swift
@@ -8,30 +8,10 @@
 import UIKit
 
 class ViewController: UIViewController {
-  private let notitficationView = NotificationSettingView()
-    override func viewDidLoad() {
+  override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
-}
-
-
-class NotificationSettingView: UIView {
-    private let imageView: UIImageView = {
-        let imageView = UIImageView()
-        imageView.image = UIImage(named: "bell")
-        imageView.tintColor = .systemPink
-        return imageView
-    }()
-    
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        addSubview(imageView)
-    }
-    required init?(coder: NSCoder) {
-        fatalError()
-    }
 }
 
